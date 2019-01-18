67fa7491ef3dd02fba6af3d265ee619415db30f4
Sal Trupiano <sal.trupiano@gmail.com>
2019-01-17 21:15:37 -0500

    R.B started on inputs and outputs

--- src/main/java/frc/robot/io/Inputs.java
+++ src/main/java/frc/robot/io/Inputs.java
@@ -1,8 +1,37 @@
 package frc.robot.io;
 
-public class Inputs{
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Inputs {
+   
+    private final Joystick controlBoard;
+        private final Joystick gamePad;
+        
     public Inputs() {
         
+        controlBoard = new Joystick(0);
+        gamePad = new Joystick(1);
+
+
+	//----------------------------------Driver Functions--------------------------------
+        public double drive; 
+        public double turnRobot;
+        public boolean driveStraight = false;
+        public boolean fieldOrintation;
+    //----------------------------Operator Functions-------------------------------------- 
+        public boolean discGather;
+        public boolean ballGather;
+        public boolean releaseBall; 
+        public boolean realeseDisc;
+        public boolean climb;
+        public boolean reverseclimb;
+        public boolean elevatorUp;
+        public boolean elevatorDown; 
+
+        public int elevatorTarget = 0;
+        
+    
+
     }
 
     public void run() {
--- src/main/java/frc/robot/io/Outputs.java
+++ src/main/java/frc/robot/io/Outputs.java
@@ -1,7 +1,44 @@
 package frc.robot.io;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class Outputs{
+    
+    private CANSparkMax frontRightMotorDrive;
+    private CANSparkMax frontRightMotorTurn;
+    private CANSparkMax frontLeftMotorDrive;
+    private CANSparkMax frontLeftMotorTurn;
+    private CANSparkMax backRightMotorDrive;
+    private CANSparkMax backRightMotorTurn;
+    private CANSparkMax backLeftMotorDrive;
+    private CANSparkMax backLeftMotorTurn;
+
+    private CANSparkMax elevatorMotor;
+
+    private CANSparkMax gatherMotor;
+    private CANSparkMax gatherArmMotor;
+
+    private CANSparkMax climbMotor;
+    
+
     public Outputs() {
+
+        frontLeftMotorDrive = new CANSparkMax(0, MotorType.kBrushless);
+        frontLeftMotorTurn = new CANSparkMax(0, MotorType.kBrushless);
+        frontRightMotorDrive = new CANSparkMax(0, MotorType.kBrushless);
+        frontRightMotorTurn = new CANSparkMax(0, MotorType.kBrushless);
+        backLeftMotorDrive = new CANSparkMax(0, MotorType.kBrushless);
+        backLeftMotorTurn = new CANSparkMax(0, MotorType.kBrushless);
+        backRightMotorTurn = new CANSparkMax(0, MotorType.kBrushless);
+        backRightMotorDrive = new CANSparkMax(0, MotorType.kBrushless);
+
+        elevatorMotor = new CANSparkMax(0, MotorType.kBrushless);
+
+        gatherMotor = new CANSparkMax(0, MotorType.kBrushless);
+        gatherArmMotor = new CANSparkMax(0, MotorType.kBrushless);
+        
+        climbMotor = new CANSparkMax(0, MotorType.kBrushless);
 
     }
     public void run() {
