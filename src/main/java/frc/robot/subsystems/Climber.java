1d2e0628e5c3701bb6b732f54c1a8c778bae3d7e
Sal Trupiano <sal.trupiano@gmail.com>
2019-01-26 12:58:26 -0500

    R.B. worked on climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,11 +1,21 @@
 package frc.robot.subsystems;
 
+import frc.robot.io.K;
+
 public class Climber extends Component{
     public Climber() {
 
     }
 
     public void run(){
-        
+        if(in.climb){
+            out.climbMotor(K.CLM_MotorSpeed);
+        }
+        else if(in.reverseclimb){
+            out.climbMotor(-K.CLM_MotorSpeed);
+        }
+        else {
+           out.climbMotor(0); 
+        }
     }
 }
