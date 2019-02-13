7dd2b3296aa0e5ca9f7ba5bc3edc4e9bca6ab397
RoboProg <info@foleyfreeze.com>
2019-02-12 20:40:04 -0500

    AKES began vision stuff

---
+++ src/main/java/frc/robot/subsystems/vision/VisionData.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.vision;
+
+//x, y, width, and height of spotted object
+
+public class VisionData {
+    public int x;
+    public int y;
+    public int w;
+    public int h;
+    public double timeStamp;
+    //subject to change
+}
