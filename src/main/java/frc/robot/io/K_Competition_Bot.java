630adb3529a4c5b014c0e17d4609bc0f95c4b707
Delta <Delta@team910.com>
2019-02-21 19:54:26 -0500

    NT Added bot sensor recognition

--- src/main/java/frc/robot/io/K_Competition_Bot.java
+++ src/main/java/frc/robot/io/K_Competition_Bot.java
@@ -4,11 +4,19 @@ public class K_Competition_Bot extends Calibrations{
   
     public K_Competition_Bot(){
         super();
-       
-        SEN_AbsAngleFL = 77.5;
-        SEN_AbsAngleFR = 278.7;
-        SEN_AbsAngleRL = 232.3;
-        SEN_AbsAngleRR = 137;
+        
+        if(Calibrations.BOT_Version == RobotType.PRACTICE){
+            SEN_AbsAngleFL = 77.5;
+            SEN_AbsAngleFR = 278.7;
+            SEN_AbsAngleRL = 232.3;
+            SEN_AbsAngleRR = 137;
+        } else {
+            SEN_AbsAngleFL = -24.4;
+            SEN_AbsAngleFR = -36.3;
+            SEN_AbsAngleRL = 82.6;
+            SEN_AbsAngleRR = -34.3;
+        }
+
 
         DRV_SwerveAngKP = -0.01;
         DRV_SwerveStrKP = -0.02;
