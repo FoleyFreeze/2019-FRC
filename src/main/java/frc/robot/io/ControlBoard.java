ff8675ceb30bfe7db295313bedc46f3f034dc613
RoboProg <info@foleyfreeze.com>
2019-03-09 15:59:01 -0500

    AKES updated button #'s for practice control Board

--- src/main/java/frc/robot/io/ControlBoard.java
+++ src/main/java/frc/robot/io/ControlBoard.java
@@ -5,28 +5,28 @@ import frc.robot.subsystems.Component;
 
 public class ControlBoard extends Component{
 
-    //2018 Practice board values!!!!!!!!!!!!!!!!!!!!!!!!
-    public int high = 4;
-    public int middle = 3;
-    public int low = 2;
-    public int front = 5;
+    //2019 Practice board values!!!!!!!!!!!!!!!!!!!!!!!!
+    public int high = 12;
+    public int middle = 11;
+    public int low = 10;
+    public int front = 9;
 
-    public int jogUp;//?????
-    public int jogDown;//?????
+    public int jogUp = 8;//?????
+    public int jogDown = 7;//?????
     
-    public int gather = 7;
-    public int shoot = 10;
+    public int gather = 1;
+    public int shoot = 4;
     
-    public int farRkt = 6;
-    public int cargoShip = 8;
-    public int nearRkt = 9;
+    public int farRkt = 15;
+    public int cargoShip = 13;
+    public int nearRkt = 14;
 
-    public int lOrR = 14;
+    public int lOrR = 5;
 
-    public int climb = 1;
+    public int climb = 6;
     
-    public int shift = 15;
-    public int pitMode = 13;
+    public int shift = 3;
+    public int pitMode = 16;
 
-    public int cargoOrHatch = 12;
+    public int cargoOrHatch = 2;
 }
