8b3a68a24895f9800a700b26893c7d49e5a60562
RoboProg <info@foleyfreeze.com>
2019-03-11 18:54:14 -0400

    making new files for autodrive

---
+++ src/main/java/frc/robot/subsystems/autodrive/Node.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.autodrive;
+
+public class Node {
+    public Point location;
+    public Polygon currPoly;
+    public Node prevNode;
+    public double totalDist;
+}
---
+++ src/main/java/frc/robot/subsystems/autodrive/Polygon.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.autodrive;
+
+public class Polygon{
+    Point[] vert;
+    Point[] edges;
+    Polygon[] neighbors;
+    boolean isDest;
+    double angle;
+
+    int id;
+
+    
+}
